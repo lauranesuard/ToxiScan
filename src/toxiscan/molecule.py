e72b9f46bbbb677a1123ab9730650c7b8445b533
lauranesuard <laurane.suard@epfl.ch>
2026-05-19 21:28:31 +0200

    modificated docstrings

--- src/toxiscan/molecule.py
+++ src/toxiscan/molecule.py
@@ -1,16 +1,19 @@
 import requests
-
 def get_smiles(molecule_name: str) -> str:
     """
     Convert a molecule name to its SMILES representation using PubChem API.
     
-    Parameters : 
-        molecule_name : str
-            the name of the molecule (e.g. "aspirin", "caffeine")
+    Parameters
+    ----------
+    molecule_name : str
+        The name of the molecule (e.g. "aspirin", "caffeine")
     
-    Returns : 
-        str : the SMILES string of the molecule, or None if not found
+    Returns
+    -------
+    str
+        The SMILES string of the molecule, or an error message if not found.
     """
+
     url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{molecule_name}/property/CanonicalSMILES/JSON"
     
     response = requests.get(url)
@@ -22,20 +25,22 @@ def get_smiles(molecule_name: str) -> str:
     else:
         return f"Molecule '{molecule_name}' not found in PubChem. Please check the name and try again."
     
-
 def get_molecule_info(molecule_name: str) -> dict:
     """
     Retrieve basic information about a molecule from PubChem.
     
-    Parameters : 
-        molecule_name : str
-            the name of the molecule (e.g. "aspirin", "caffeine")
+    Parameters
+    ----------
+    molecule_name : str
+        The name of the molecule (e.g. "aspirin", "caffeine")
     
-    Returns :
-        dict
-            A dictionary containing basic molecular information,
-            or an error message if the molecule is not found.
+    Returns
+    -------
+    dict
+        A dictionary containing basic molecular information,
+        or an error message if the molecule is not found.
     """
+    
     url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{molecule_name}/property/ConnectivitySMILES,MolecularFormula,MolecularWeight,IUPACName/JSON"
 
     response = requests.get(url)
